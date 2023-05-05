9f3b95a7f9f228ae6e0ee6efb32e6fda8efc92a1
Os Sm <os583369@gmail.com>
2023-05-05 16:52:28 +0100

    main build

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,55 @@
-import { Component, ViewChild } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Component, HostListener } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
-
 })
 export class AppComponent {
-  title = 'image-viewer';
-  applyForm = new FormGroup({
-    url: new FormControl('')
-  });
 
-  constructor() {
-
-    console.log("In app.component constructor");
-
+  @HostListener('window:keydown.enter', ['$event'])
+  enterHandler(event: any) {
+    console.log("key pressed");
   }
 
+  @HostListener('window:keydown.arrowright', ['$event'])
+  rightHandler(event: any) {
+    console.log("right arrow key pressed");
+    if (this.pointer + 1 < this.imageUrls.length ) {
+      this.pointer++;
+      this.setImage();
+    }
+  }
+
+  @HostListener('window:keydown.arrowleft', ['$event'])
+  leftHandler(event: any) {
+    console.log("left key pressed");
+    if (this.pointer > 0 ) {
+      this.pointer--;
+      this.setImage();
+    }
+  }
+
+  constructor() {
+    console.log("In app.component constructor");
+  }
+
+  title = 'image-viewer';
+  imageUrl: string = "";
+  pointer = 0;
+  private imageUrls = new Array<string>();
+
+  applyForm = new FormGroup({
+    urlForm: new FormControl('')
+  });
+
   submitApplication() {
-    let targetUrl = <string>this.applyForm.value.url;
-    let url = "http://localhost:8080/" + targetUrl;
+    let targetUrl = <string>this.applyForm.value.urlForm;
+    let appendUrl = "http://localhost:8080/";
+    let url = appendUrl + targetUrl;
     let domain = new URL(targetUrl).hostname;
-    
+
     console.log("url: " + url);
 
     let xhr = new XMLHttpRequest();
@@ -31,15 +57,41 @@ export class AppComponent {
 
     xhr.onreadystatechange = () => {
       if (xhr.readyState === 4) {
-        let dom = new DOMParser().parseFromString(xhr.responseText, 'text/html')
+        let dom = new DOMParser().parseFromString(xhr.responseText, 'text/html');
         if (dom != null) {
           let links = dom.getElementsByTagName("a");
           for (let i = 0; i < links.length; i++) {
             let anchor = links[i];
             let href = anchor.getAttribute("href");
             if (href?.startsWith("/photo")) {
-              console.log(domain + href);
+              let level2Url = "https://" + domain + href;
+              // console.log("level2: " + level2Url);
+              let xhr2 = new XMLHttpRequest();
+              xhr2.open('GET', appendUrl + level2Url, true);
+              xhr2.onreadystatechange = () => {
+                // console.log("readystate: " + xhr2.readyState);
+                if (xhr2.readyState === 4) {
+                  let dom2 = new DOMParser().parseFromString(xhr2.responseText, 'text/html');
+                  if (dom2 != null) {
+                    let links2 = dom2.getElementsByTagName("a");
 
+                    for (let i2 = 0; i2 < links2.length; i2++) {
+                      let anchor2 = links2[i2];
+                      let href2 = anchor2.getAttribute("href");
+                      // console.log("href2: " + href2);
+                      if (href2?.includes("full")) {
+                        // console.log("anchor2: " + href2);
+                        // first image
+                        this.imageUrls.push(href2);
+                        if (this.imageUrls.length === 1) {
+                          this.setImage()
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+              xhr2.send();
             }
 
           }
@@ -47,7 +99,11 @@ export class AppComponent {
       }
     }
 
-    xhr.send()
+    xhr.send();
+
   }
 
+  setImage() {
+    this.imageUrl = this.imageUrls[this.pointer];
+  }
 }
