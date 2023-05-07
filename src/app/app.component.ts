c8dbd6f92f83e4303f445398eb704600594e9944
Os Sm <os583369@gmail.com>
2023-05-07 21:48:00 +0100

    some files changed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,8 +22,8 @@ export class AppComponent {
 
   @HostListener('window:keydown.arrowright', ['$event'])
   rightHandler(event: any) {
-    console.log("right arrow key pressed. count: " + this.imageUrls.length.toString());
-    if (this.pointer + 1 < this.imageUrls.length) {
+    console.log("right arrow key pressed. count: " + this.photoPages.length.toString());
+    if (this.pointer + 1 < this.photoPages.length) {
       this.pointer++;
       this.setImage();
     }
@@ -49,7 +49,7 @@ export class AppComponent {
   pointer = 0;
   log = "";
   private photoPages = new Array<HTMLElement>();
-  private imageUrls = new Array<string>();
+  // private imageUrls = new Array<string>();
   private httpClient: HttpClient;
 
   applyForm = new FormGroup({
@@ -57,8 +57,7 @@ export class AppComponent {
   });
 
   processFile() {
-    let appendUrl = "http://localhost:8080/";
-    let domain = "www.imagefap.com";
+
     this.httpClient.get('assets/source.txt', { responseType: 'text' })
       .subscribe((data) => {
         // console.log(data);
@@ -67,152 +66,73 @@ export class AppComponent {
         if (dom != null) {
           let links = dom.getElementsByTagName("a");
           console.log("anchors: " + links.length);
-          let found1 = false;
 
           for (let i = 0; i < links.length; i++) {
             let anchor = <HTMLAnchorElement>links[i];
             let href = anchor.getAttribute("href");
             if (href?.startsWith("/photo")) {
-              //found1 = true; // for testing
               // console.log("href1: " + href);
               let exp = /photo\/(.*)\//;
               let matches = exp.exec(href);
               if (matches != null) {
                 let photoId = matches[1];
                 // console.log("photoId: " + photoId.toString());
-                if (anchor != null && ! this.hasAlready(anchor)) {
+                if (anchor != null && !this.hasAlready(anchor)) {
                   this.photoPages.push(anchor);
 
                 }
-                // let level2Url = "https://" + domain + href;
-                // console.log("level2: " + level2Url);
-                // let xhr2 = new XMLHttpRequest();
-                // xhr2.open('GET', appendUrl + level2Url, true);
-                // xhr2.onreadystatechange = () => {
-                //   // console.log("readystate: " + xhr2.readyState);
-                //   if (xhr2.readyState === 4) {
-                //     let dom2 = new DOMParser().parseFromString(xhr2.responseText, 'text/html');
-                //     if (dom2 != null) {
-                //       let links2 = dom2.getElementsByTagName("a");
-                //       let found2 = false;
-                //       for (let i2 = 0; i2 < links2.length; i2++) {
-                //         let anchor2 = links2[i2];
-                //         let href2 = anchor2.getAttribute("href");
-                //         if (href2?.includes("full") &&
-                //           href2?.includes(photoId)) {
-                //           console.log("href2: " + href2);
-                //           found2 = true;
-                //           console.log("anchor2: " + href2);
-                //           // first image
-                //           this.imageUrls.push(href2);
-                //           if (this.imageUrls.length === 1) {
-                //             this.setImage()
-                //           }
-                //         }
-                //         if (found2) {
 
-                //           break;
-                //         }
-                //       }
-                //     }
-                //   }
-                // }
-                // xhr2.send();
               }
-            }
-            if (found1) {
-              break;
             }
           }
           console.log("photo pages: " + this.photoPages.length);
+          this.setImage();
         }
       });
 
   }
 
 
-  submitApplication() {
-    let targetUrl = <string>this.applyForm.value.urlForm;
+
+
+  setImage() {
     let appendUrl = "http://localhost:8080/";
-    let url = appendUrl + targetUrl;
-    let domain = new URL(targetUrl).hostname;
+    let domain = "www.imagefap.com";
+    let href = this.photoPages[this.pointer].getAttribute("href");
+    let photoId = this.photoPages[this.pointer].getAttribute("name");
 
-    console.log("url: " + url);
-
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET', url, true);
-
-    // xhr.onreadystatechange = () => {
-    xhr.onloadend = () => {
-      // if (xhr.readyState === 4) {
-      this.log = xhr.responseText;
-      let dom = new DOMParser().parseFromString(xhr.responseText, 'text/html');
-
-      if (dom != null) {
-        let links = dom.getElementsByTagName("a");
-        console.log("anchors: " + links.length);
-        let found1 = false;
-        for (let i = 0; i < links.length; i++) {
-          let anchor = links[i];
-          let href = anchor.getAttribute("href");
-          if (href?.startsWith("/photo")) {
-            // found1 = true;
-            console.log("href1: " + href);
-            let exp = /photo\/(.*)\//;
-            let matches = exp.exec(href);
-            if (matches != null) {
-              let photoId = matches[1];
-              console.log("photoId: " + photoId.toString());
-              let level2Url = "https://" + domain + href;
-              // console.log("level2: " + level2Url);
-              let xhr2 = new XMLHttpRequest();
-              xhr2.open('GET', appendUrl + level2Url, true);
-              xhr2.onreadystatechange = () => {
-                // console.log("readystate: " + xhr2.readyState);
-                if (xhr2.readyState === 4) {
-                  let dom2 = new DOMParser().parseFromString(xhr2.responseText, 'text/html');
-                  if (dom2 != null) {
-                    let links2 = dom2.getElementsByTagName("a");
-                    let found2 = false;
-                    for (let i2 = 0; i2 < links2.length; i2++) {
-                      let anchor2 = links2[i2];
-                      let href2 = anchor2.getAttribute("href");
-                      if (href2?.includes("full") &&
-                        href2?.includes(photoId)) {
-                        console.log("href2: " + href2);
-                        found2 = true;
-                        console.log("anchor2: " + href2);
-                        // first image
-                        this.imageUrls.push(href2);
-                        if (this.imageUrls.length === 1) {
-                          this.setImage()
-                        }
-                      }
-                      if (found2) {
-
-                        break;
-                      }
-                    }
-                  }
-                }
-              }
-              xhr2.send();
+    let url = "https://" + domain + href;
+    console.log("level2: " + url);
+    let xhr2 = new XMLHttpRequest();
+    xhr2.open('GET', appendUrl + url, true);
+    xhr2.onreadystatechange = () => {
+      // console.log("readystate: " + xhr2.readyState);
+      if (xhr2.readyState === 4) {
+        let dom2 = new DOMParser().parseFromString(xhr2.responseText, 'text/html');
+        if (dom2 != null && photoId != null) {
+          let links2 = dom2.getElementsByTagName("a");
+          let found2 = false;
+          for (let i2 = 0; i2 < links2.length; i2++) {
+            let anchor2 = links2[i2];
+            let href2 = anchor2.getAttribute("href");
+            if (href2?.includes("full") &&
+              href2?.includes(photoId)) {
+              console.log("href2: " + href2);
+              found2 = true;
+              console.log("anchor2: " + href2);
+              
             }
-          }
-          if (found1) {
-            break;
+            if (found2) {
+
+              break;
+            }
           }
         }
       }
-      // }
     }
+    xhr2.send();
 
-    xhr.send();
-
-  }
-
-  setImage() {
-    this.imageUrl = this.imageUrls[this.pointer];
+    //this.imageUrl = this.imageUrls[this.pointer];
   }
 
   hasAlready(elNeedle: HTMLAnchorElement): boolean {
