49d3caf8cd8244a3da91864d3aae26f1b7be6602
Os Sm <os583369@gmail.com>
2023-05-07 20:22:15 +0100

    some files changed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -55,67 +55,68 @@ export class AppComponent {
     let xhr = new XMLHttpRequest();
     xhr.open('GET', url, true);
 
-    xhr.onreadystatechange = () => {
-      if (xhr.readyState === 4) {
-        let dom = new DOMParser().parseFromString(xhr.responseText, 'text/html');
+    // xhr.onreadystatechange = () => {
+    xhr.onloadend = () => {
+      // if (xhr.readyState === 4) {
+      let dom = new DOMParser().parseFromString(xhr.responseText, 'text/html');
 
-        if (dom != null) {
-          let links = dom.getElementsByTagName("a");
-          console.log("anchors: " + links.length);
-          let found1 = false;
-          for (let i = 0; i < links.length; i++) {
-            let anchor = links[i];
-            let href = anchor.getAttribute("href");
-            if (href?.startsWith("/photo")) {
-              // found1 = true;
-              console.log("href1: " + href);
-              let exp = /photo\/(.*)\//;
-              let matches = exp.exec(href);
-              if (matches != null) {
-                let photoId = matches[1];
-                console.log("photoId: " + photoId.toString());
-                let level2Url = "https://" + domain + href;
-                // console.log("level2: " + level2Url);
-                let xhr2 = new XMLHttpRequest();
-                xhr2.open('GET', appendUrl + level2Url, true);
-                xhr2.onreadystatechange = () => {
-                  // console.log("readystate: " + xhr2.readyState);
-                  if (xhr2.readyState === 4) {
-                    let dom2 = new DOMParser().parseFromString(xhr2.responseText, 'text/html');
-                    if (dom2 != null) {
-                      let links2 = dom2.getElementsByTagName("a");
-                      let found2 = false;
-                      for (let i2 = 0; i2 < links2.length; i2++) {
-                        let anchor2 = links2[i2];
-                        let href2 = anchor2.getAttribute("href");
-                        if (href2?.includes("full") &&
+      if (dom != null) {
+        let links = dom.getElementsByTagName("a");
+        console.log("anchors: " + links.length);
+        let found1 = false;
+        for (let i = 0; i < links.length; i++) {
+          let anchor = links[i];
+          let href = anchor.getAttribute("href");
+          if (href?.startsWith("/photo")) {
+            // found1 = true;
+            console.log("href1: " + href);
+            let exp = /photo\/(.*)\//;
+            let matches = exp.exec(href);
+            if (matches != null) {
+              let photoId = matches[1];
+              console.log("photoId: " + photoId.toString());
+              let level2Url = "https://" + domain + href;
+              // console.log("level2: " + level2Url);
+              let xhr2 = new XMLHttpRequest();
+              xhr2.open('GET', appendUrl + level2Url, true);
+              xhr2.onreadystatechange = () => {
+                // console.log("readystate: " + xhr2.readyState);
+                if (xhr2.readyState === 4) {
+                  let dom2 = new DOMParser().parseFromString(xhr2.responseText, 'text/html');
+                  if (dom2 != null) {
+                    let links2 = dom2.getElementsByTagName("a");
+                    let found2 = false;
+                    for (let i2 = 0; i2 < links2.length; i2++) {
+                      let anchor2 = links2[i2];
+                      let href2 = anchor2.getAttribute("href");
+                      if (href2?.includes("full") &&
                         href2?.includes(photoId)) {
-                          console.log("href2: " + href2);
-                          found2 = true;
-                          console.log("anchor2: " + href2);
-                          // first image
-                          this.imageUrls.push(href2);
-                          if (this.imageUrls.length === 1) {
-                            this.setImage()
-                          }
+                        console.log("href2: " + href2);
+                        found2 = true;
+                        console.log("anchor2: " + href2);
+                        // first image
+                        this.imageUrls.push(href2);
+                        if (this.imageUrls.length === 1) {
+                          this.setImage()
                         }
-                        if (found2) {
+                      }
+                      if (found2) {
 
-                          break;
-                        }
+                        break;
                       }
                     }
                   }
                 }
-                xhr2.send();
               }
+              xhr2.send();
             }
-            if (found1) {
-              break;
-            }
+          }
+          if (found1) {
+            break;
           }
         }
       }
+      // }
     }
 
     xhr.send();
